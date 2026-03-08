473e2ec2304c7ccac6ad6b5198cb5728b9350a28
sittheinaung-dev <sittheinaung76@gmail.com>
2026-03-08 10:09:32 +0630

    feat: Pagination

--- IPB2.ApiDreamDictionary/Controllers/DreamDictionaryController.cs
+++ IPB2.ApiDreamDictionary/Controllers/DreamDictionaryController.cs
@@ -35,60 +35,86 @@ namespace IPB2.ApiDreamDictionary.Controllers
             return Ok(BlogDetail);
 
         }
-        [HttpGet("Search")]
-        public IActionResult Search(string keyword)
-        {
-            if (string.IsNullOrWhiteSpace(keyword))
-                return BadRequest("Keyword is required");
-
-            var blogHeaders = _db.BlogHeaders
-                .Where(x => EF.Functions.Like(x.BlogTitle, $"%{keyword}%"))
-                .ToList();
-
-            var blogDetails = _db.Blogdetails
-                .Where(x => EF.Functions.Like(x.BlogContent, $"%{keyword}%"))
-                .ToList();
-
-            var result = new
-            {
-                Headers = blogHeaders,
-                Details = blogDetails
-            };
-
-            return Ok(result);
-        }
-
         //[HttpGet("Search")]
         //public IActionResult Search(string keyword)
         //{
         //    if (string.IsNullOrWhiteSpace(keyword))
         //        return BadRequest("Keyword is required");
 
-        //    var headerResults = _db.BlogHeaders
+        //    var blogHeaders = _db.BlogHeaders
         //        .Where(x => EF.Functions.Like(x.BlogTitle, $"%{keyword}%"))
-        //        .Select(x => new
-        //        {
-        //            Type = "Header",
-        //            Id = x.BlogId,
-        //            Text = x.BlogTitle
-        //        });
-
-        //    var detailResults = _db.Blogdetails
-        //        .Where(x => EF.Functions.Like(x.BlogContent, $"%{keyword}%"))
-        //        .Select(x => new
-        //        {
-        //            Type = "Detail",
-        //            Id = x.BlogDetailId,
-        //            Text = x.BlogContent
-        //        });
-
-        //    var result = headerResults
-        //        .Union(detailResults)
         //        .ToList();
+
+        //    var blogDetails = _db.Blogdetails
+        //        .Where(x => EF.Functions.Like(x.BlogContent, $"%{keyword}%"))
+        //        .ToList();
+
+        //    var result = new
+        //    {
+        //        Headers = blogHeaders,
+        //        Details = blogDetails
+        //    };
 
         //    return Ok(result);
         //}
 
+        [HttpGet("Search")]
+        public IActionResult Search(string keyword)
+        {
+            if (string.IsNullOrWhiteSpace(keyword))
+                return BadRequest("Keyword is required");
+
+            var headerResults = _db.BlogHeaders
+                .Where(x => EF.Functions.Like(x.BlogTitle, $"%{keyword}%"))
+                .Select(x => new
+                {
+                    Type = "Header",
+                    Id = x.BlogId,
+                    Text = x.BlogTitle
+                });
+
+            var detailResults = _db.Blogdetails
+                .Where(x => EF.Functions.Like(x.BlogContent, $"%{keyword}%"))
+                .Select(x => new
+                {
+                    Type = "Detail",
+                    Id = x.BlogDetailId,
+                    Text = x.BlogContent
+                });
+
+            var result = headerResults
+                .Union(detailResults)
+                .ToList();
+
+            return Ok(result);
+        }
+        [HttpGet("List")]
+        public IActionResult GetBlogList(int pageNo = 1, int pageSize = 10)
+        {
+            if (pageNo <= 0) pageNo = 1;
+            if (pageSize <= 0) pageSize = 10;
+
+            var query = _db.BlogHeaders.AsQueryable();
+
+            var totalCount = query.Count();
+
+            var data = query
+                .OrderBy(x => x.BlogId)
+                .Skip((pageNo - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var result = new
+            {
+                PageNo = pageNo,
+                PageSize = pageSize,
+                TotalCount = totalCount,
+                TotalPages = (int)Math.Ceiling((double)totalCount / pageSize),
+                Data = data
+            };
+
+            return Ok(result);
+        }
 
     }
 }
